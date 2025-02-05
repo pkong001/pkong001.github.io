ede8e4c216cccf5d74e5b335f3cf817d1adc1773
Pongpisut Kongdan <Pongpisut.kon@bjc.co.th>
2025-02-05 08:04:49 +0700

    update about about about about about me

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// function loadHeader() {
-//     fetch('header.html')
-//         .then(response => response.text())
-//         .then(data => {
-//             document.getElementById('header').innerHTML = data;
-//             document.getElementById("page-nav").innerHTML = "this is the new one";
-//         });
-// } อันนี้ก็ใช้ได้นะฮะ ที่ต้อแอด <body onload="loadHeader()">
-
-
 function loadheader() {
     fetch('./component/header.html')
     .then(response => response.text())
@@ -16,9 +6,6 @@ function loadheader() {
         document.getElementById("page-nav").innerHTML = content_navigator;
     });
 }
-// function changeText() {
-//     document.getElementById("tester").innerHTML = "This is the new text";
-// }
 
 function footer1() {
     fetch('./component/footer1.html')
